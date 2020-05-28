faf8c381d33a8bb3da8c4d909aef578544044d33
filmscore88 <cmtmorales24@gmail.com>
2020-05-28 14:45:31 -0500

    uninstalled react bootstraped swaped for materialui

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -1,8 +1,7 @@
 import React from "react";
 import heroLogo from "../Media/gmapsinworkout.png"; // Tell webpack this JS file uses this image
 import SignUpButton from "./SignUpButton";
-import { Image } from "react-bootstrap/Image";
-
+import Image from "react-bootstrap/Image";
 const Hero = () => {
   // Import result is the URL of your image
   return (
