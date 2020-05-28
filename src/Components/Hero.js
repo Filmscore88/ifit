3ddbb950d5b8c485a9d83cb0fb323cd45477b82d
filmscore88 <cmtmorales24@gmail.com>
2020-05-28 17:31:29 -0500

    refactored png image to display text

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -1,19 +1,77 @@
 import React from "react";
 import heroLogo from "../Media/gmapsinworkout.png"; // Tell webpack this JS file uses this image
 import SignUpButton from "./SignUpButton";
-import Image from "react-bootstrap/Image";
-const Hero = () => {
-  // Import result is the URL of your image
+import CardMedia from "@material-ui/core/CardMedia";
+import { withStyles } from "@material-ui/core/styles";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import { Card, CardContent } from "@material-ui/core";
+const styles = theme => ({
+  card: {
+    display: "flex"
+  },
+  details: {
+    display: "flex",
+    flexDirection: "column"
+  },
+  content: {
+    flex: "1 0 auto"
+  },
+  cover: {
+    width: 800
+  },
+  controls: {
+    display: "flex",
+    alignItems: "center",
+    paddingLeft: theme.spacing.unit,
+    paddingBottom: theme.spacing.unit
+  },
+  container: {
+    position: "relative",
+    textAlign: "center"
+  },
+  topasad: {
+    position: "absolute",
+    top: "10%",
+    left: "50%"
+  },
+
+  media: {
+    display: "flex",
+    height: 100,
+    objectFit: "contain",
+    alignItems: "left"
+  }
+});
+
+function Hero(props) {
+  const { classes } = props;
   return (
-    <div>
-      <Image
-        src={heroLogo}
-        style={{ width: "1240px", height: "601px" }}
-        alt="Logo"
-      >
-        <SignUpButton />
-      </Image>
-    </div>
+    <Card className={classes.card}>
+      <div className={classes.con}>
+        <CardContent className={classes.content}>
+          <div style={{ position: "relative" }}>
+            <CardMedia
+              component="img"
+              className={classes.media}
+              src={heroLogo}
+            />
+            <div
+              style={{
+                position: "absolute",
+                color: "white",
+                top: 8,
+                left: "50%",
+                transform: "translateX(-50%)"
+              }}
+            >
+              <SignUpButton />
+            </div>
+          </div>
+        </CardContent>
+      </div>
+    </Card>
   );
-};
-export default Hero;
+}
+
+// export default Header
+export default withStyles(styles)(Hero);
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -5,3 +5,31 @@ const Home = () => {
 };
 
 export default Home;
+
+// function Hero(props) {
+//   const { classes } = props;
+//   return (
+//     <Card className={classes.card}>
+//       <div className={classes.con}>
+//         <CardContent className={classes.content}>
+//           <div style={{ position: "relative" }}>
+//             <CardMedia>
+//               <img src={heroLogo} />
+//             </CardMedia>
+//             <div
+//               style={{
+//                 position: "absolute",
+//                 color: "white",
+//                 top: 8,
+//                 left: "50%",
+//                 transform: "translateX(-50%)"
+//               }}
+//             >
+//               Your text
+//             </div>
+//           </div>
+//         </CardContent>
+//       </div>
+//     </Card>
+//   );
+// }
